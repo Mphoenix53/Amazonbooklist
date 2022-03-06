207e23924e5b013b1cb7454629b52249b5399f84
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-03-06 12:24:43 -0700

    routes file done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
  
  namespace :api do
+
+  get    '/authors',         to:"authors#index"
+  get    '/authors/:id',     to:"authors#show"
+  post   '/authors',         to:"authors#create"
+  put    '/authors/:id',     to:"authors#update"
+  delete '/authors/:id',     to:"authors#destroy"
+
  end
 
 end
